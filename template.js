19ab6bc7e4b3d5e0dfbe33ce09e47da9f5f3d56e
cis101 <cis101@secs.iub.edu.bd>
2019-07-29 09:46:26 +0600

    signup

--- template.js
+++ template.js
@@ -1,13 +1,15 @@
 export default () => {
-    return `<!doctype html>
-      <html lang="en">
-        <head>
-          <meta charset="utf-8">
-          <title>MERN Skeleton</title>
-          <link rel="stylesheet" href="/public/style.css">
-        </head>
-        <body>
-          <div id="root">Hello World</div>
-        </body>
-      </html>`
+  return `<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>MERN Skeleton</title>
+        <link rel="stylesheet" href="/public/style.css">
+      </head>
+      <body>
+        <div id="root">Hello World</div>
+        <a href="/signup">Sign Up</a><br/>
+        <a href="/signin">Sign In</a>
+      </body>
+    </html>`
 }
